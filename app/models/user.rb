a14645a23aeb3c4da885363233de3b392d99ca11
Yotam Rosenbaum <yotamrosen@gmail.com>
2014-11-25 15:56:57 -0800

    fix error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, message: "Can't create a record without a valid email"
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 255, message: "Can't create a record without a valid email" },
                     format: { with: VALID_EMAIL_REGEX, message: "Invalid email address" },
                     uniqueness: { case_sensitive: false, message: "Email already exists in the system" }
                     
