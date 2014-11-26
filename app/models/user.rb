7f3f396abe83e9c9ed7177e846f3de71ac4353fa
Thomas Davis <thomasalwyndavis@gmail.com>
2014-11-26 12:11:23 +1000

    added geocoder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  
+  geocoded_by :ip_address,
+    :latitude => :lat, :longitude => :lon
+  after_validation :geocode
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255, message: "Can't create a record without a valid email" },
                     format: { with: VALID_EMAIL_REGEX, message: "Invalid email address" },
